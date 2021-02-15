4f1649b7ab3f9e8713000156013d46a78064ff6a
Hannah <64833265+iHannah-01@users.noreply.github.com>
2021-02-15 21:33:45 +0000

    new changes to nav bar (not working yet)

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
 <button onclick="location.href='about.html'">here</button>
 
+  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
 
